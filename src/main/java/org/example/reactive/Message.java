32acb010b6ff35b00694ceff277894ef04dcfac1
abhishekshrestha <tech.abhishek@groupiig.com>
2025-07-14 15:21:57 +0545

    added the main method in MyFlux.java

--- src/main/java/org/example/reactive/Message.java
+++ src/main/java/org/example/reactive/Message.java
@@ -1,6 +1,10 @@
 package org.example.reactive;
 
 public class Message {
+  private Message() {
+    throw new IllegalStateException("Utility class");
+  }
+
   static final String RECEIVED = "Received: ";
   static final String ERROR = "Error: ";
   static final String COMPLETED = "Completed";
--- src/main/java/org/example/reactive/MyFlux.java
+++ src/main/java/org/example/reactive/MyFlux.java
@@ -3,8 +3,26 @@ package org.example.reactive;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.logging.Logger;
 
 public class MyFlux {
+
+  private static final Logger logger = Logger.getLogger(MyFlux.class.getName());
+
+  public static void main(String[] args) throws InterruptedException {
+    MyFlux myFlux = new MyFlux();
+    myFlux.getEmptyFlux().subscribe(value -> logger.info(String.valueOf(value)));
+    myFlux.getFluxFromValues("A", "B", "C").subscribe(logger::info);
+    myFlux.getFluxFromIterable(List.of("X", "Y", "Z")).subscribe(logger::info);
+    myFlux
+        .errorFlux()
+        .subscribe(
+            value -> logger.info(String.valueOf(value)),
+            error -> logger.severe("Error: " + error.getMessage()));
+    myFlux.counter().subscribe(value -> logger.info(String.valueOf(value)));
+    Thread.sleep(2000);
+  }
 
   Flux<String> getEmptyFlux() {
     return Flux.empty();
