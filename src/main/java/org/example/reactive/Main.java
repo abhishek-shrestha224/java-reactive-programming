1d3bf48d2249ce9fef48b66bfed45c9db7cfd2fc
abhishekshrestha <tech.abhishek@groupiig.com>
2025-07-14 15:13:22 +0545

    undo changes in Main.java to only have hello world

--- src/main/java/org/example/reactive/Main.java
+++ src/main/java/org/example/reactive/Main.java
@@ -1,25 +1,11 @@
 package org.example.reactive;
 
 import java.util.logging.Logger;
-import reactor.core.publisher.Flux;
 
 public class Main {
   private static final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-  public static void main(String[] args) throws InterruptedException {
-    MyFlux fluxCreator = new MyFlux();
-    Flux<Long> flux;
-
-    //    flux = fluxCreator.getEmptyFlux();
-    //    flux = fluxCreator.getFluxFromValues("Hello", "World", "from", "Reactor");
-    //    flux = fluxCreator.getFluxFromIterable(Arrays.asList("Hello", "World", "from",
-    // "Reactor"));
-    flux = fluxCreator.counter();
-    flux.subscribe(
-        i -> logger.info("onNext Triggered: " + i),
-        error -> logger.severe("onError Triggered: " + error.getMessage()),
-        () -> logger.info("onComplete Triggered"));
-
-    Thread.sleep(2000);
+  public static void main(String[] args) {
+    logger.info("Hello World!");
   }
 }
